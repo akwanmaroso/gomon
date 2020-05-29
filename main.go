0179f1072ff93ece1dea53d0af043f5b7db67a60
Akwan Maroso <akwanmaroso@gmail.com>
2020-05-29 15:35:48 +0800

    perf: add watching files and directories

--- main.go
+++ main.go
@@ -1,9 +1,50 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/fsnotify/fsnotify"
 )
 
+var watcher *fsnotify.Watcher
+
 func main() {
-	log.Fatal("Hello")
+	// create a new watcher
+	watcher, _ = fsnotify.NewWatcher()
+	defer watcher.Close()
+	fmt.Println("helo")
+	pwd, err := os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(pwd)
+
+	if err := filepath.Walk(pwd, watchDir); err != nil {
+		fmt.Println("ERROR", err)
+	}
+	done := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case event := <-watcher.Events:
+				fmt.Printf("ERROR! %#v\n", event)
+
+			case err := <-watcher.Errors:
+				fmt.Println("ERROR", err)
+			}
+		}
+	}()
+
+	<-done
+}
+
+func watchDir(path string, fi os.FileInfo, err error) error {
+	if fi.Mode().IsDir() {
+		return watcher.Add(path)
+	}
+	return nil
 }
